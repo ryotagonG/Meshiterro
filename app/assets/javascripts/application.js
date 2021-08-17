581f9d26e9adea06fbff7b184fb56168f4d52211
ryotagonG <ryotapb715@gmail.com>
2021-08-17 09:46:09 +0000

    updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,8 +8,12 @@
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+// about supported directives.s
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '30da21ce14aaf8eed3b32ad5b66bf927fa8db997b560b01a8e250642b0a089927f3f176d310ae94caafa2279a56b0441f83fdd7f7dd222edeb577a8791e16eb0'
